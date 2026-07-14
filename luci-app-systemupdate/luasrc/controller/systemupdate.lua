e824dcd36f3054e00e461c75d4b0dbfdad17aa0b
root <root@WIN-VS1EU6CPR1I.localdomain>
2026-07-14 19:50:02 +0800

    fix(systemupdate): rewrite to pure template, fix XHR endpoint URL

--- luci-app-systemupdate/luasrc/controller/systemupdate.lua
+++ luci-app-systemupdate/luasrc/controller/systemupdate.lua
@@ -1,19 +1,19 @@
 module("luci.controller.systemupdate", package.seeall)
 
 function index()
-	entry({"admin", "system", "systemupdate"}, cbi("systemupdate/update"), _("系统更新"), 90)
+	entry({"admin", "system", "systemupdate"}, template("systemupdate/update"), _("系统更新"), 90)
 	entry({"admin", "system", "systemupdate", "action"}, call("action_update"), nil).dependent = false
 end
 
 function action_update()
 	local action = luci.http.formvalue("action")
 	local mode = luci.http.formvalue("mode") or "fresh"
-	
+
+	luci.http.prepare_content("text/plain")
+
 	if action == "check" then
-		luci.http.prepare_content("text/plain")
 		luci.http.write(luci.sys.exec("/usr/bin/systemupdate check"))
 	elseif action == "update" then
-		luci.http.prepare_content("text/plain")
 		if mode == "keep" then
 			luci.http.write(luci.sys.exec("/usr/bin/systemupdate keep"))
 		else
