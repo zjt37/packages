ecefa64e10dd006b78b4703888a2297b11f62196
root <root@WIN-VS1EU6CPR1I.localdomain>
2026-07-14 20:59:38 +0800

    fix(systemupdate): change menu priority to 80, above reboot

--- luci-app-systemupdate/luasrc/controller/systemupdate.lua
+++ luci-app-systemupdate/luasrc/controller/systemupdate.lua
@@ -1,5 +1,5 @@
 module("luci.controller.systemupdate", package.seeall)
 
 function index()
-	entry({"admin", "system", "systemupdate"}, template("systemupdate/update"), _("系统更新"), 90)
+	entry({"admin", "system", "systemupdate"}, template("systemupdate/update"), _("系统更新"), 80)
 end
