50f70a3e335e03ce63c787f60be316a7d8fa2397
zjt37 <27096491@qq.com>
2026-08-21 01:50:38 +0000

    luci-app-nodeagg: keep url row inside same section

--- luci-app-nodeagg/luasrc/model/cbi/nodeagg/settings.lua
+++ luci-app-nodeagg/luasrc/model/cbi/nodeagg/settings.lua
@@ -10,7 +10,11 @@ o = s:option(Flag, "enabled", translate("启用"))
 o.default = 0
 o.rmempty = false
 
-s2 = Map("nodeagg")
-s2.template = "nodeagg/agg_url_row"
+o = s:option(DummyValue, "agg_url_row", " ")
+o.rawhtml = true
+o.cfgvalue = function(self, section)
+	local url = m.uci:get("nodeagg", section, "agg_url") or "http://192.168.2.30/nodeagg.txt"
+	return '<div class="cbi-value"><label class="cbi-value-title">' .. translate("聚合地址") .. '</label><div class="cbi-value-field"><input type="text" id="nodeagg_agg_url" value="' .. url .. '" readonly style="width:300px;" /> <input type="button" class="cbi-button" value="' .. translate("复制") .. '" onclick="var u=document.getElementById(\'nodeagg_agg_url\').value;navigator.clipboard.writeText(u).then(function(){alert(\'已复制: \'+u)})" /></div></div>'
+end
 
-return m, s2
+return m
