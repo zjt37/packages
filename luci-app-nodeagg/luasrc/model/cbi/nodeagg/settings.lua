5ed3ed99abebcc466966c6b2dbd38fdb3592c4bd
zjt37 <27096491@qq.com>
2026-08-21 01:46:25 +0000

    luci-app-nodeagg: render url and button in same row

--- luci-app-nodeagg/luasrc/model/cbi/nodeagg/settings.lua
+++ luci-app-nodeagg/luasrc/model/cbi/nodeagg/settings.lua
@@ -10,19 +10,13 @@ o = s:option(Flag, "enabled", translate("启用"))
 o.default = 0
 o.rmempty = false
 
-o = s:option(Value, "agg_url", translate("聚合地址"))
-o.readonly = true
-o.placeholder = "http://192.168.2.30/nodeagg.txt"
-o.datatype = "url"
-
-o = s:option(Button, "_copy")
-o.title = " "
-o.inputtitle = translate("复制")
-o.inputstyle = nil
-o.onclick = function(self)
-	local url = m.uci:get("nodeagg", "@nodeagg[0]", "agg_url") or ""
-	luci.http.prepare_content("text/plain")
-	luci.http.write(url)
+o = s:option(DummyValue, "agg_url_display", " ")
+o.rawhtml = true
+o.cfgvalue = function(self, section)
+	local url = m.uci:get("nodeagg", section, "agg_url") or "http://192.168.2.30/nodeagg.txt"
+	return '<label for="agg_url">' .. translate("聚合地址") .. ':</label> ' ..
+		'<input type="text" id="nodeagg_agg_url" name="nodeagg.agg_url" value="' .. url .. '" readonly style="width:300px;" /> ' ..
+		'<input type="button" class="cbi-button" value="' .. translate("复制") .. '" onclick="var u=document.getElementById(\'nodeagg_agg_url\').value;navigator.clipboard.writeText(u).then(function(){alert(\'已复制: \'+u)})" />'
 end
 
 return m
