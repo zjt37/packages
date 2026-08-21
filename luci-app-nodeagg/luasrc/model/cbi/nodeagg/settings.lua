e10d4a3f1bfa3ac9ba94ec605606d9c13292bdfa
zjt37 <27096491@qq.com>
2026-08-21 02:14:16 +0000

    luci-app-nodeagg: add subscribe list section

--- luci-app-nodeagg/luasrc/model/cbi/nodeagg/settings.lua
+++ luci-app-nodeagg/luasrc/model/cbi/nodeagg/settings.lua
@@ -2,6 +2,7 @@ local m, s, o
 
 m = Map("nodeagg", translate("聚合"))
 
+-- 聚合
 s = m:section(TypedSection, "nodeagg")
 s.anonymous = true
 s.addremove = false
@@ -20,5 +21,20 @@ o.cfgvalue = function(self, section)
 	local url = m.uci:get("nodeagg", section, "agg_url") or "http://192.168.2.30/nodeagg.txt"
 	return '<input type="button" class="cbi-button" value="' .. translate("复制") .. '" onclick="var t=document.createElement(\'textarea\');t.textContent=\'' .. url .. '\';t.style.position=\'fixed\';document.body.appendChild(t);t.select();try{document.execCommand(\'copy\');alert(\'' .. translate("已复制") .. ': \'+t.textContent)}catch(e){alert(\'copy failed\')}finally{document.body.removeChild(t)}" />'
 end
+
+-- 订阅源
+s = m:section(TypedSection, "subscribe_list", translate("订阅源"))
+s.addremove = true
+s.anonymous = true
+s.sortable = true
+s.template = "cbi/tblsection"
+
+o = s:option(Value, "remark", translate("备注"))
+o.width = "auto"
+o.rmempty = false
+
+o = s:option(Value, "url", translate("订阅地址"))
+o.width = "auto"
+o.rmempty = false
 
 return m
