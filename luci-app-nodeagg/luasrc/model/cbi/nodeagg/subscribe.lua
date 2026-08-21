077a4cf7e4734952400bdb5ec33e1d2734f9580c
zjt37 <27096491@qq.com>
2026-08-21 03:36:31 +0000

    luci-app-nodeagg: revert to passwall API version (ecd28c1)

--- luci-app-nodeagg/luasrc/model/cbi/nodeagg/subscribe.lua
+++ luci-app-nodeagg/luasrc/model/cbi/nodeagg/subscribe.lua
@@ -1,133 +1,227 @@
-local sys = require("luci.sys")
+local api = require "luci.passwall.api"
+api.set_default_cbi()
 
-local function cmd_exists(cmd)
-	return sys.exec('echo -n $(type -t -p "%s" 2>/dev/null)' % cmd) ~= ""
+local has_ss_rust = api.is_finded("sslocal")
+local has_singbox = api.finded_com("sing-box")
+local has_xray = api.finded_com("xray")
+local has_hysteria2 = api.finded_com("hysteria")
+local ss_type = {}
+local trojan_type = {}
+local vmess_type = {}
+local vless_type = {}
+local hysteria2_type = {}
+local xray_version = api.get_app_version("xray")
+if has_ss_rust then
+	local s = "shadowsocks-rust"
+	table.insert(ss_type, s)
+end
+if has_singbox then
+	local s = "sing-box"
+	table.insert(trojan_type, s)
+	table.insert(ss_type, s)
+	table.insert(vmess_type, s)
+	table.insert(vless_type, s)
+	table.insert(hysteria2_type, s)
+end
+if has_xray then
+	local s = "xray"
+	table.insert(trojan_type, s)
+	table.insert(ss_type, s)
+	table.insert(vmess_type, s)
+	table.insert(vless_type, s)
+	if api.compare_versions(xray_version, ">=", "26.1.13") then
+		table.insert(hysteria2_type, s)
+	end
+end
+if has_hysteria2 then
+	local s = "hysteria2"
+	table.insert(hysteria2_type, s)
 end
 
-local has_singbox = cmd_exists("sing-box")
-local has_xray = cmd_exists("xray")
-local has_hysteria2 = cmd_exists("hysteria2") or cmd_exists("hysteria")
+m = Map()
 
-m = Map("nodeagg")
-
--- [[ 订阅设置 ]]--
-s = m:section(TypedSection, "nodeagg")
-s.anonymous = true
-s.addremove = false
-
-o = s:option(ListValue, "filter_keyword_mode", translate("过滤关键词模式"))
-o:value("0", translate("关闭"))
-o:value("1", translate("丢弃列表"))
-o:value("2", translate("保留列表"))
-o:value("3", translate("丢弃列表,但保留列表优先"))
-o:value("4", translate("保留列表,但丢弃列表优先"))
-
-o = s:option(DynamicList, "filter_discard_list", translate("丢弃列表"))
-
-o = s:option(DynamicList, "filter_keep_list", translate("保留列表"))
-
-if has_singbox or has_xray then
-	o = s:option(ListValue, "ss_type", translate("%s 节点使用类型", "Shadowsocks"))
-	o:value("", translate("自动"))
-	if has_singbox then o:value("sing-box", "sing-box") end
-	if has_xray then o:value("xray", "xray") end
+function m.on_before_save(self)
+	self:foreach("subscribe_list", function(e)
+		self:del(e[".name"], "md5")
+	end)
 end
 
-if has_singbox or has_xray then
-	o = s:option(ListValue, "trojan_type", translate("%s 节点使用类型", "Trojan"))
-	o:value("", translate("自动"))
-	if has_singbox then o:value("sing-box", "sing-box") end
-	if has_xray then o:value("xray", "xray") end
+-- [[ Subscribe Settings ]]--
+s = m:section(NamedSection, "@global_subscribe[0]", "global_subscribe")
+
+o = s:option(ListValue, "filter_keyword_mode", translate("Filter keyword Mode"))
+o:value("0", translate("Close"))
+o:value("1", translate("Discard List"))
+o:value("2", translate("Keep List"))
+o:value("3", translate("Discard List,But Keep List First"))
+o:value("4", translate("Keep List,But Discard List First"))
+
+o = s:option(DynamicList, "filter_discard_list", translate("Discard List"))
+
+o = s:option(DynamicList, "filter_keep_list", translate("Keep List"))
+
+if #ss_type > 0 then
+	o = s:option(ListValue, "ss_type", translatef("%s Node Use Type", "Shadowsocks"))
+	o:value("", translate("Auto"))
+	for key, value in pairs(ss_type) do
+		o:value(value)
+	end
 end
 
-if has_singbox or has_xray then
-	o = s:option(ListValue, "vmess_type", translate("%s 节点使用类型", "VMess"))
-	o:value("", translate("自动"))
-	if has_singbox then o:value("sing-box", "sing-box") end
-	if has_xray then o:value("xray", "xray") end
+if #trojan_type > 0 then
+	o = s:option(ListValue, "trojan_type", translatef("%s Node Use Type", "Trojan"))
+	o:value("", translate("Auto"))
+	for key, value in pairs(trojan_type) do
+		o:value(value)
+	end
 end
 
-if has_singbox or has_xray then
-	o = s:option(ListValue, "vless_type", translate("%s 节点使用类型", "VLESS"))
-	o:value("", translate("自动"))
-	if has_singbox then o:value("sing-box", "sing-box") end
-	if has_xray then o:value("xray", "xray") end
+if #vmess_type > 0 then
+	o = s:option(ListValue, "vmess_type", translatef("%s Node Use Type", "VMess"))
+	o:value("", translate("Auto"))
+	for key, value in pairs(vmess_type) do
+		o:value(value)
+	end
 end
 
-if has_singbox or has_xray or has_hysteria2 then
-	o = s:option(ListValue, "hysteria2_type", translate("%s 节点使用类型", "Hysteria2"))
-	o:value("", translate("自动"))
-	if has_singbox then o:value("sing-box", "sing-box") end
-	if has_xray then o:value("xray", "xray") end
-	if has_hysteria2 then o:value("hysteria2", "hysteria2") end
+if #vless_type > 0 then
+	o = s:option(ListValue, "vless_type", translatef("%s Node Use Type", "VLESS"))
+	o:value("", translate("Auto"))
+	for key, value in pairs(vless_type) do
+		o:value(value)
+	end
 end
 
----- 删除所有订阅节点
-o = s:option(DummyValue, "_stop", translate("删除所有订阅节点"))
+if #hysteria2_type > 0 then
+	o = s:option(ListValue, "hysteria2_type", translatef("%s Node Use Type", "Hysteria2"))
+	o:value("", translate("Auto"))
+	for key, value in pairs(hysteria2_type) do
+		o:value(value)
+	end
+end
+
+if #ss_type > 0 or #trojan_type > 0 or #vmess_type > 0 or #vless_type > 0 or #hysteria2_type > 0 then
+	o.description = string.format("<font color='red'>%s</font>",
+			translate("The configured type also applies to the core specified when manually importing nodes."))
+end
+
+---- Subscribe Delete All
+o = s:option(DummyValue, "_stop", translate("Delete All Subscribe Node"))
+o.rawhtml = true
+function o.cfgvalue(self, section)
+	return string.format(
+		[[<input type="button" class="btn cbi-button cbi-button-remove" onclick="return confirmDeleteAll()" value="%s" />]],
+		translate("Delete All Subscribe Node"))
+end
+
+o = s:option(DummyValue, "_update", translate("Manual subscription All"))
 o.rawhtml = true
 o.cfgvalue = function(self, section)
-	return '<input type="button" class="btn cbi-button cbi-button-remove" onclick="confirmDeleteAll()" value="' .. translate("删除所有订阅节点") .. '" />'
+    return string.format([[
+        <input type="button" class="btn cbi-button cbi-button-apply" onclick="ManualSubscribeAll()" value="%s" />]],
+	 translate("Manual subscription All"))
 end
 
----- 手动订阅所有
-o = s:option(DummyValue, "_update", translate("手动订阅所有"))
-o.rawhtml = true
-o.cfgvalue = function(self, section)
-	return '<input type="button" class="btn cbi-button cbi-button-apply" onclick="ManualSubscribeAll()" value="' .. translate("手动订阅所有") .. '" />'
-end
-
--- [[ 订阅列表 ]]--
-s = m:section(TypedSection, "subscribe_list", "", '<font color="red">' .. translate("添加新订阅后请保存并应用，再手动订阅。如果只修改了订阅地址，可以手动订阅，系统会自动保存。") .. '</font>')
+s = m:section(TypedSection, "subscribe_list", "", "<font color='red'>" .. translate("When adding a new subscription, please save and apply before manually subscribing. If you only change the subscription URL, you can subscribe manually, and the system will save it automatically.") .. "</font>")
 s.addremove = true
 s.anonymous = true
 s.sortable = true
 s.template = "cbi/tblsection"
 s.extedit = luci.dispatcher.build_url("admin", "services", "nodeagg", "subscribe_edit", "%s")
 function s.create(e, t)
-	local uid = "sub_" .. sys.exec("echo -n $(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 5)")
+	local uid = "sub_" .. api.gen_random_char(5)
 	TypedSection.create(e, uid)
+	m:set(uid, "hysteria_up_mbps", "100")
+	m:set(uid, "hysteria_down_mbps", "100")
 	luci.http.redirect(e.extedit:format(uid))
 end
 
-o = s:option(Value, "remark", translate("备注"))
+o = s:option(Value, "remark", translate("Remarks"))
 o.width = "auto"
 o.rmempty = false
-
-o = s:option(DummyValue, "_node_count", translate("订阅信息"))
-o.rawhtml = true
-o.cfgvalue = function(self, section)
-	return translate("节点数量") .. ": 0"
+o.validate = function(self, value, section)
+	value = api.trim(value)
+	if value == "" then
+		return nil, translate("Remark cannot be empty.")
+	end
+	local duplicate = false
+	m:foreach(s.sectiontype, function(e)
+		if e[".name"] ~= section and e["remark"] and e["remark"]:lower() == value:lower() then
+			duplicate = true
+			return false
+		end
+	end)
+	if duplicate or value:lower() == "default" then
+		return nil, translate("This remark already exists, please change a new remark.")
+	end
+	return value
+end
+o.write = function(self, section, value)
+	local old = m:get(section, self.option) or ""
+	if old ~= value then
+		m:foreach("nodes", function(e)
+			if e["group"] and e["group"]:lower() == old:lower() then
+				m:set(e[".name"], "group", value)
+			end
+			if e["protocol"] and (e["protocol"] == "_balancing" or e["protocol"] == "_urltest") and e["node_group"] then
+				local gs = ""
+				for g in e["node_group"]:gmatch("%S+") do
+					if api.UrlEncode(old) == g then
+						gs = gs .. " " .. api.UrlEncode(value)
+					else
+						gs = gs .. " " .. g
+					end
+				end
+				gs = api.trim(gs)
+				m:set(e[".name"], "node_group", gs)
+			end
+		end)
+	end
+	return Value.write(self, section, value)
 end
 
-o = s:option(Value, "url", translate("订阅地址"))
+o = s:option(DummyValue, "_node_count", translate("Subscribe Info"))
+o.rawhtml = true
+o.cfgvalue = function(t, n)
+	local remark = m:get(n, "remark") or ""
+	local str = m:get(n, "rem_traffic") or ""
+	local expired_date = m:get(n, "expired_date") or ""
+	if expired_date ~= "" then
+		str = str .. (str ~= "" and "/" or "") .. expired_date
+	end
+	str = str ~= "" and "<br>" .. str or ""
+	local num = 0
+	m:foreach("nodes", function(s)
+		if s["group"] and s["group"]:lower() == remark:lower() then
+			num = num + 1
+		end
+	end)
+	return string.format("%s%s", translate("Node num") .. ": " .. num, str)
+end
+
+o = s:option(Value, "url", translate("Subscribe URL"))
 o.width = "auto"
 o.rmempty = false
 
-o = s:option(DummyValue, "_remove", translate("删除订阅节点"))
+o = s:option(DummyValue, "_remove", translate("Delete the subscribed node"))
 o.rawhtml = true
-o.cfgvalue = function(self, section)
+function o.cfgvalue(self, section)
 	local remark = m:get(section, "remark") or ""
-	return '<input type="button" class="btn cbi-button cbi-button-remove" onclick="confirmDeleteNode(\'' .. remark .. '\')" value="' .. translate("删除订阅节点") .. '" />'
+	return string.format(
+		[[<input type="button" class="btn cbi-button cbi-button-remove" onclick="return confirmDeleteNode('%s')" value="%s" />]],
+		remark, translate("Delete the subscribed node"))
 end
 
-o = s:option(DummyValue, "_update2", translate("手动订阅"))
+o = s:option(DummyValue, "_update", translate("Manual subscription"))
 o.rawhtml = true
 o.cfgvalue = function(self, section)
-	return '<input type="button" class="btn cbi-button cbi-button-apply" onclick="ManualSubscribe(\'' .. section .. '\')" value="' .. translate("手动订阅") .. '" />'
+    return string.format([[
+        <input type="button" class="btn cbi-button cbi-button-apply" onclick="ManualSubscribe('%s')" value="%s" />]],
+	section, translate("Manual subscription"))
 end
 
-s2 = m:section(TypedSection, "nodeagg")
-s2.anonymous = true
-s2.addremove = false
-o = s2:option(DummyValue, "_js", " ")
-o.rawhtml = true
-o.cfgvalue = function(self, section)
-	return '<script type="text/javascript">'
-		.. 'function confirmDeleteNode(remark){if(!confirm("' .. translate("删除订阅节点") .. ': "+remark+" ?"))return false;return true;}'
-		.. 'function confirmDeleteAll(){if(!confirm("' .. translate("确定要删除所有订阅节点吗？") .. '"))return false;return true;}'
-		.. 'function ManualSubscribe(sectionId){var urlInput=document.querySelector("input[name=\'cbid.nodeagg."+sectionId+".url\']");var currentUrl=urlInput?urlInput.value.trim():"";if(!currentUrl){alert("' .. translate("订阅地址不能为空") .. '");return;}alert("' .. translate("手动订阅") .. ': "+currentUrl);}'
-		.. 'function ManualSubscribeAll(){alert("' .. translate("手动订阅所有") .. '");}'
-		.. '</script>'
-end
+m:appendTemplate("/cbi/sortable", {sectiontype = s.sectiontype})
+
+m:appendTemplate("/node_subscribe/js")
 
 return m
