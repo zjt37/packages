1e5734338f7abc1db4e98a522f65861a44f69b7f
zjt37 <27096491@qq.com>
2026-08-21 01:31:30 +0000

    luci-app-nodeagg: add aggregation section with toggle and url

--- luci-app-nodeagg/luasrc/model/cbi/nodeagg/settings.lua
+++ luci-app-nodeagg/luasrc/model/cbi/nodeagg/settings.lua
@@ -1,14 +1,18 @@
 local m, s, o
 
-m = Map("nodeagg", translate("基本设置"),
-	translate("节点聚合与订阅转换的基本配置"))
+m = Map("nodeagg", translate("节点聚合"))
 
-s = m:section(TypedSection, "nodeagg")
+-- 聚合
+s = m:section(TypedSection, "nodeagg", translate("聚合"))
 s.anonymous = true
 s.addremove = false
 
 o = s:option(Flag, "enabled", translate("启用"))
 o.default = 0
 o.rmempty = false
+
+o = s:option(Value, "agg_url", translate("聚合地址"))
+o.readonly = true
+o.placeholder = "http://192.168.2.30:8080/nodeagg"
 
 return m
