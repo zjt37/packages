48cf8f0be17e412eaf598bd1211de79b8730f2bc
zjt37 <27096491@qq.com>
2026-08-21 06:28:51 +0000

    luci-app-nodeagg: add node_list settings and fix passwall refs

--- luci-app-nodeagg/luasrc/model/cbi/nodeagg/node_list.lua
+++ luci-app-nodeagg/luasrc/model/cbi/nodeagg/node_list.lua
@@ -1,2 +1,21 @@
-local m = Map("nodeagg")
+local api = require "luci.nodeagg.api"
+api.set_default_cbi()
+
+m = Map("nodeagg")
+m.redirect = luci.dispatcher.build_url("admin", "services", "nodeagg", "node_list")
+
+-- [[ Other Settings ]]--
+s = m:section(TypedSection, "global_other")
+s.anonymous = true
+
+o = s:option(ListValue, "auto_detection_time", translate("自动检测延迟"))
+o:value("0", translate("关闭"))
+o:value("icmp", "Ping")
+o:value("tcping", "TCP Ping")
+
+o = s:option(Flag, "show_node_info", translate("显示服务器地址和端口"))
+o.default = "0"
+
+m:appendTemplate("nodeagg/node_list/node_list")
+
 return m
