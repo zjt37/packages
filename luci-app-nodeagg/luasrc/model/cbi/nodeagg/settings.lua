1f3e0ab880650dd89cb99ed2e8f2bd1cb9c2b20e
zjt37 <27096491@qq.com>
2026-08-21 02:04:52 +0000

    luci-app-nodeagg: restore copy button with querySelector

--- luci-app-nodeagg/luasrc/model/cbi/nodeagg/settings.lua
+++ luci-app-nodeagg/luasrc/model/cbi/nodeagg/settings.lua
@@ -10,11 +10,14 @@ o = s:option(Flag, "enabled", translate("启用"))
 o.default = 0
 o.rmempty = false
 
-o = s:option(DummyValue, "agg_url_display", " ")
+o = s:option(Value, "agg_url", translate("聚合地址"))
+o.readonly = true
+o.placeholder = "http://192.168.2.30/nodeagg.txt"
+
+o = s:option(DummyValue, "_copy", " ")
 o.rawhtml = true
 o.cfgvalue = function(self, section)
-	local url = m.uci:get("nodeagg", section, "agg_url") or "http://192.168.2.30/nodeagg.txt"
-	return '<div class="cbi-value"><label class="cbi-value-title">' .. translate("聚合地址") .. '</label><div class="cbi-value-field"><input type="text" id="nodeagg_agg_url" value="' .. url .. '" readonly style="width:300px;" /> <input type="button" class="cbi-button" value="' .. translate("复制") .. '" onclick="var u=document.getElementById(\'nodeagg_agg_url\').value;navigator.clipboard.writeText(u).then(function(){alert(\'已复制: \'+u)})" /></div></div>'
+	return '<input type="button" class="cbi-button" value="' .. translate("复制") .. '" onclick="var u=document.querySelector(\'[id$=_agg_url]\').value;navigator.clipboard.writeText(u).then(function(){alert(\'已复制: \'+u)})" />'
 end
 
 return m
