43744dcd652f87599c3cb3496f56d375a251f626
zjt37 <27096491@qq.com>
2026-08-21 04:56:01 +0000

    luci-app-nodeagg: copy passwall node_list page to subscribe tab

--- luci-app-nodeagg/luasrc/controller/nodeagg.lua
+++ luci-app-nodeagg/luasrc/controller/nodeagg.lua
@@ -4,7 +4,7 @@ function index()
 	entry({"admin", "services", "nodeagg"}, alias("admin", "services", "nodeagg", "settings"), _("节点聚合"), 60).dependent = true
 
 	entry({"admin", "services", "nodeagg", "settings"}, cbi("nodeagg/settings"), _("节点聚合"), 1).dependent = true
-	entry({"admin", "services", "nodeagg", "node_list"}, cbi("nodeagg/node_list"), _("节点列表"), 2).dependent = true
-	entry({"admin", "services", "nodeagg", "subscribe"}, cbi("nodeagg/subscribe"), _("节点订阅"), 3).dependent = true
+	entry({"admin", "services", "nodeagg", "node_list"}, cbi("nodeagg/node_list"), _("节点订阅"), 2).dependent = true
+	entry({"admin", "services", "nodeagg", "subscribe"}, cbi("nodeagg/subscribe"), _("节点列表"), 3).dependent = true
 	entry({"admin", "services", "nodeagg", "subscribe_edit"}, cbi("nodeagg/subscribe_edit")).leaf = true
 end
