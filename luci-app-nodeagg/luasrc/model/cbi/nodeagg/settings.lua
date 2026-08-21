ccb39bc210484e8c97094bfde58f87a48bbc4d26
zjt37 <27096491@qq.com>
2026-08-21 01:55:01 +0000

    luci-app-nodeagg: fix copy button JS selector

--- luci-app-nodeagg/luasrc/model/cbi/nodeagg/settings.lua
+++ luci-app-nodeagg/luasrc/model/cbi/nodeagg/settings.lua
@@ -17,7 +17,14 @@ o.placeholder = "http://192.168.2.30/nodeagg.txt"
 o = s:option(DummyValue, "_script", " ")
 o.rawhtml = true
 o.cfgvalue = function(self, section)
-	return '<script type="text/javascript">document.addEventListener("DOMContentLoaded",function(){var u=document.querySelector("[id$=_agg_url]");if(u){var b=document.createElement("input");b.type="button";b.className="cbi-button";b.value="' .. translate("复制") .. '";b.onclick=function(){navigator.clipboard.writeText(u.value).then(function(){alert("' .. translate("已复制") .. ': "+u.value)})};u.parentNode.appendChild(b)}});</script>'
+	return '<script type="text/javascript">//<![CDATA[\n' ..
+		'var initCopy=function(){var inputs=document.querySelectorAll("input[type=text]");' ..
+		'for(var i=0;i<inputs.length;i++){if(inputs[i].value.indexOf("nodeagg")!==-1||inputs[i].id.indexOf("agg_url")!==-1){' ..
+		'var b=document.createElement("input");b.type="button";b.className="cbi-button";b.value="' .. translate("复制") .. '";' ..
+		'b.onclick=(function(input){return function(){navigator.clipboard.writeText(input.value).then(function(){alert("' .. translate("已复制") .. ': "+input.value)})}})(inputs[i]);' ..
+		'inputs[i].parentNode.appendChild(b);break}}};' ..
+		'if(document.readyState==="loading"){document.addEventListener("DOMContentLoaded",initCopy)}else{initCopy()}\n' ..
+		'//]]></script>'
 end
 
 return m
