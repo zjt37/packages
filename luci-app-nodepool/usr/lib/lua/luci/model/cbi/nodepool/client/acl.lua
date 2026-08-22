e370856c779b65a0cb1317413fcaf29b4bb4fc78
zjt37 <27096491@qq.com>
2026-08-22 07:28:00 +0000

    luci-app-nodepool: add package (node aggregation + subscription conversion)

---
+++ luci-app-nodepool/usr/lib/lua/luci/model/cbi/nodepool/client/acl.lua
@@ -0,0 +1,85 @@
+local api = require "luci.nodepool.api"
+
+api.set_default_cbi()
+
+m = Map()
+
+s = m:section(NamedSection, "@global[0]", "global", translate("ACLs"), "<font color='red'>" .. translate("ACLs is a tools which used to designate specific IP proxy mode.") .. "</font>")
+
+o = s:option(Flag, "acl_enable", translate("Main switch"))
+o.rmempty = false
+o.default = false
+
+-- [[ ACLs Settings ]]--
+s = m:section(TypedSection, "acl_rule")
+s.template = "cbi/tblsection"
+s.sortable = true
+s.anonymous = true
+s.addremove = true
+s.extedit = api.url("acl_config", "%s")
+function s.create(e, t)
+	local uid = "acl_" .. api.gen_random_char(5)
+	TypedSection.create(e, uid)
+	luci.http.redirect(e.extedit:format(uid))
+end
+function s.remove(e, t)
+	api.sys.call("rm -rf /tmp/etc/nodepool_tmp/dns_" .. t .. "*")
+	TypedSection.remove(e, t)
+end
+
+---- Enable
+o = s:option(Flag, "enabled", translate("Enable"))
+o.default = 1
+o.rmempty = false
+
+---- Remarks
+o = s:option(Value, "remarks", translate("Remarks"))
+o.rmempty = true
+
+local mac_t = {}
+api.sys.net.mac_hints(function(e, t)
+	mac_t[e] = {
+		ip = t,
+		mac = e
+	}
+end)
+
+i = s:option(DummyValue, "interface", translate("Source Interface"))
+i.cfgvalue = function(t, n)
+	local v = Value.cfgvalue(t, n) or ''
+	if v == "" then
+		return translate("All")
+	end
+	return v
+end
+
+o = s:option(DummyValue, "sources", translate("Source"))
+o.rawhtml = true
+o.cfgvalue = function(t, n)
+	local e = ''
+	local v = Value.cfgvalue(t, n) or '-'
+	string.gsub(v, '[^' .. " " .. ']+', function(w)
+		local a = w
+		if mac_t[w] then
+			a = a .. ' (' .. mac_t[w].ip .. ')'
+		end
+		if #e > 0 then
+			e = e .. "<br />"
+		end
+		e = e .. a
+	end)
+	return e
+end
+
+i = s:option(DummyValue, "mode", translate("Mode"))
+i.cfgvalue = function(t, n)
+	local v = Value.cfgvalue(t, n) or '0'
+	if v == "1" then
+		return translate("Proxy")
+	end
+	return translate("No Proxy")
+end
+
+m:appendTemplate("/cbi/sortable", {sectiontype = s.sectiontype})
+
+return api.return_map(m)
---
+++ luci-app-nodepool/usr/lib/lua/luci/model/cbi/nodepool/client/node_config.lua
@@ -0,0 +1,116 @@
+api = require "luci.nodepool.api"
+api.set_default_cbi()
+
+m = Map()
+m.redirect = api.url("node_list")
+
+if not arg[1] or not m:get(arg[1]) then
+	luci.http.redirect(m.redirect)
+end
+
+formvalue_key = "cbid." .. m.config .. "." .. arg[1] .. "."
+
+m:appendTemplate("/node_config/header", {section = arg[1]})
+m:appendTemplate("/cbi/nodes_multivalue_com")
+m:appendTemplate("/cbi/nodes_listvalue_com")
+
+groups = {}
+m:foreach("nodes", function(s)
+	if s[".name"] ~= arg[1] then
+		if s.group and s.group ~= "" then
+			groups[s.group] = true
+		end
+	end
+end)
+
+local s = m:section(NamedSection, arg[1], "nodes", translate("Node Config"))
+s.addremove = false
+s.dynamic = false
+
+o = s:option(DummyValue, "nodepool", "　")
+o.rawhtml  = true
+o.template = m:template_path("/node_config/link_share_man")
+o.value = arg[1]
+
+o = s:option(Value, "remarks", translate("Node Remarks"))
+o.default = translate("Remarks")
+o.rmempty = false
+
+o = s:option(Value, "group", translate("Group Name"))
+o.default = ""
+o:value("", translate("default"))
+for k, v in pairs(groups) do
+	o:value(k)
+end
+o.write = function(self, section, value)
+	value = api.trim(value)
+	local lower = value:lower()
+
+	if lower == "" or lower == "default" then
+		return m:del(section, self.option)
+	end
+
+	for _, v in ipairs(self.keylist or {}) do
+		if v:lower() == lower then
+			return m:set(section, self.option, v)
+		end
+	end
+	m:set(section, self.option, value)
+end
+
+local types_dir = "/usr/lib/lua/luci/model/cbi/" .. api.appname .. "/client/type/"
+s.val = {}
+s.val["type"] = m:get(arg[1], "type")
+s.val["protocol"] = m:get(arg[1], "protocol")
+
+if luci.http.formvalue("cbi.submit") == "1" then
+	local formvalue_type = luci.http.formvalue(formvalue_key .. "type")
+	if formvalue_type then
+		s.val["type"] = formvalue_type
+	end
+end
+
+o = s:option(ListValue, "type", translate("Type"))
+
+if api.is_finded("ipt2socks") then
+	local type_name = "Socks"
+
+	s.fields["type"]:value(type_name, "Socks")
+
+	if s.val["type"] == type_name then
+		local s2 = NamedSection(m, arg[1], "server")
+		s2.type_name = type_name
+		s2.option_prefix = "socks_"
+
+		o = s2:option(ListValue, "del_protocol", "　") --始终隐藏，用于删除 protocol
+		o:depends({ __hide = "1" })
+		o.rewrite_option = "protocol"
+
+		o = s2:option(Value, "address", translate("Address (Support Domain Name)"))
+
+		o = s2:option(Value, "port", translate("Port"))
+		o.datatype = "port"
+
+		o = s2:option(Value, "username", translate("Username"))
+
+		o = s2:option(Value, "password", translate("Password"))
+		o.password = true
+
+		api.luci_types(s, s2)
+	end
+end
+
+local type_table = {}
+for filename in api.fs.dir(types_dir) do
+	table.insert(type_table, filename)
+end
+table.sort(type_table)
+
+for index, value in ipairs(type_table) do
+	local p_func = loadfile(types_dir .. value)
+	setfenv(p_func, getfenv(1))(m, s)
+end
+
+m:appendTemplate("/node_config/footer", {section = arg[1]})
+
+return api.return_map(m)
